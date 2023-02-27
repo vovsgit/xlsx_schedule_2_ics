743aa626bb1e4315aec26ecb351d2d795a78a2c9
“YVov <nroot@vov.su>
2023-02-28 00:36:45 +0300

    try delete hidden rows

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from typing import List
 import openpyxl.workbook
+from openpyxl import Workbook
 
 
 def check_args(args: List[str]) -> List[str]:
@@ -11,7 +12,7 @@ def check_args(args: List[str]) -> List[str]:
         raise ValueError("Invalid number of arguments")
 
 
-def get_files(group: str) -> List[openpyxl.workbook]:
+def get_files(group: str) -> List[Workbook]:
     dir_path = './schedules'
 
     if group == 'all':
@@ -21,10 +22,25 @@ def get_files(group: str) -> List[openpyxl.workbook]:
         files = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if group in f]
         return [openpyxl.load_workbook(files[0])]
 
-def parse_workbook(workbook: openpyxl.workbook) -> List[List[str]]:
+def parse_workbook(workbook: Workbook) -> List[List[str]]:
     sheet = workbook.active
     data = []
-    for row in sheet.iter_rows(min_row=2, max_col=7, values_only=True):
+
+    #hidden_rows = [row_idx for row_idx, row in enumerate(sheet.rows) if row[0].hidden]
+
+    # Удаление скрытых строк
+    for row in reversed(list(sheet.rows)):
+        if row[0].hidden:
+            sheet.delete_rows(row[0].row)
+
+    groups = sheet.cell(row=2, column=2).value
+    speciality = sheet.cell(row=3, column=2).value
+
+    hidden_cells = [(cell.row, cell.column) for cell in sheet._cells if cell.has_style('hidden')]
+
+    disciplines = [sheet.cell(row=9 + i, column=2).value for i in range(0, 27, 3)]
+
+    for row in sheet.iter_rows(min_row=1, max_col=71, values_only=True):
         data.append(row)
     return data
 
@@ -32,3 +48,5 @@ if __name__ == "__main__":
     args = sys.argv
     group, english_group = check_args(args)
     workbooks = get_files(group)
+    parse_workbook(workbooks[0])
+
